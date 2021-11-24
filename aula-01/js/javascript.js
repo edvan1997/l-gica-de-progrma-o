990926a94f932c1afa877e67988f5f91bbea2511
edvan1997 <edvinvi0000@gmail.com>
2021-11-24 19:35:09 -0300

    atualizado

--- aula-01/js/javascript.js
+++ aula-01/js/javascript.js
@@ -32,3 +32,88 @@ function sair(){
     // caixa.style.fontWeight = '900'
 
 }
+
+//Pegar a data atual do computador
+let agora = new Date();
+//Pega o dia da semana em valores numéricos
+let diaSemana = agora.getDay();
+//Pega o ID da Div
+let data = document.getElementById('data')
+
+// switch(diaSemana){
+
+//     case 0:
+//         data.innerHTML = 'Domingo';
+//         data.style.background = 'blue'
+//     break;
+
+//     case 1:
+//          data.innerHTML = 'Segunda-feira';
+//          data.style.background = 'grey'
+//     break;
+
+//     case 2:
+//          data.innerHTML = 'Terça-feira';
+//          data.style.background = 'pink'
+//     break;
+
+//     case 3:
+//          data.innerHTML = 'Quarta-feira';
+//          data.style.background = 'black'
+//     break;
+    
+//     case 4:
+//          data.innerHTML = 'Quinta-feira';
+//          data.style.background = 'orange'
+//     break;
+    
+//     case 5:
+//          data.innerHTML = 'Sexta-feira';
+//          data.style.background = 'yellow'
+//     break;
+
+//     case 6:
+//          data.innerHTML = 'Sábado';
+//          data.style.background = 'green'
+//     break;
+
+//     default:
+//          console.log('Dia inválido');
+//     break;
+// }
+
+if(diaSemana == 0){
+    data.innerHTML = ('Domingo');
+    data.style.background = ('blue');
+}else if(diaSemana == 1){
+    data.innerHTML =('Segunda');
+    data.style.background = ('orange');
+}else if(diaSemana == 2){
+    data.innerHTML = ('Terça-feira');
+    data.style.background = ('black');
+}else if(diaSemana == 3){
+    data.innerHTML = ('Quarta-feira');
+    data.style.background =('grey')
+}else if(diaSemana == 4){
+    data.InnerHTML = ('Quinta-feira');
+    data.style.background=('pink');
+}else if(diaSemana == 5){
+    data.innerHTML = ('Sexta-feira');
+    data.style.background = ('yellow');
+}else if(diaSemana == 6){
+    data.innerHTML = ('Sábado');
+    data.style.background = ('red');
+}else{
+    data.innerHTML = ('Data inválida');
+    data.style.background =('white')
+    data.style.color = ('black')
+}
+
+
+let cont = 0;
+let contDiv = document.getElementById('cont');
+
+while(cont < 50){
+    cont++;
+    contDiv.innerHTML = (cont);
+}
